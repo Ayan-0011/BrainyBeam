4cd9aac7fee4482b2abd34bb0ad9d9d5e4f78d54
Ayan_0011 <ayanansari9235@gmail.com>
2026-07-08 15:23:05 +0530

    Task-2 done

--- TASK-01/src/Components/SerchBar.jsx
+++ TASK-01/src/Components/SerchBar.jsx
@@ -8,8 +8,7 @@ const SearchBar = ({ search, setSearch }) => {
         <Search size={20}  className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-500" />
 
         <input type="text" placeholder="Search your favorite food..."
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
+          value={search} onChange={(e) => setSearch(e.target.value)}
           className="w-full pl-12 pr-4 py-3 border border-gray-300 rounded-xl outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500" />
       </div>
     </div>
---
+++ TASK-02/src/Timer.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+
+const Timer = () => {
+  const [seconds, setSeconds] = useState("");
+  const [timeLeft, setTimeLeft] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+
+  const handleStart = () => {
+
+    if (!seconds || seconds <= 0) return;
+
+    setTimeLeft(Number(seconds));
+    setIsRunning(true);
+  };
+
+  useEffect(() => {
+    
+
+    const timer = setInterval(() => {
+      setTimeLeft((current) => {
+
+        if (current <= 1) {
+          clearInterval(timer);
+          setIsRunning(false);
+          return 0;
+        }
+
+        return current - 1;
+      });
+    }, 1000);
+
+    setSeconds("")
+
+    return () => clearInterval(timer);
+  }, [isRunning]);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-xl shadow-lg w-96">
+        <h1 className="text-3xl font-bold text-center mb-6">
+          Digital Kitchen Timer
+        </h1>
+
+        <input type="number" placeholder="Enter seconds" value={seconds}
+          onChange={(e) => setSeconds(e.target.value)}
+          className="w-full border p-3 rounded-lg mb-4" />
+
+        <button onClick={handleStart}
+          className="w-full bg-blue-500 text-white py-3 rounded-lg active:scale-95  hover:bg-blue-600" >
+          Start Timer
+        </button>
+
+        <div className="text-center mt-8">
+          <h2 className="text-6xl font-bold text-green-600">
+            {timeLeft}
+          </h2>
+
+          {timeLeft === 0 && !isRunning && (
+            <p className="text-green-600 mt-4 text-lg font-semibold">
+              🎉 Time's Up!
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Timer;
