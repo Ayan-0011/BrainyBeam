b4263cfc247a07022600dd83efc7a4ee48784eb0
Ayan_0011 <ayanansari9235@gmail.com>
2026-09-07 11:27:51 +0530

    Starting nextjs routing

--- NextJS/my-app/app/about/page.tsx
+++ NextJS/my-app/app/about/page.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link';
+
 export default function page() {
   return (
     <div>
-      <div className="text-center bg-gray-800 text-text-white p-5 text-2xl">
+      <div className="text-center bg-red-800 text-text-white p-5 text-2xl">
         <h1>About Page</h1>
       </div>
+      <p><Link href="about/blog" className='p-2 text-blue-500'>Blog</Link></p>
     </div>
   )
 }
