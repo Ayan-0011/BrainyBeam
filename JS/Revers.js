e5bf503a8cdd690f746f066d2659a42ac1311c68
Ayan_0011 <ayanansari9235@gmail.com>
2026-09-15 23:20:05 +0530

    ps in js

--- JS/Revers.js
+++ JS/Revers.js
@@ -3,9 +3,24 @@ function revers(arr) {
     let rev = [];
 
     for (i = arr.length; i >= 0; i--) {
-        rev += arr[i]
+        rev.push(arr[i])
     }
     return rev
 }
 
 console.log(revers([1, 2, 3, 4, 5]));
+
+
+function revers(str) {
+
+    let rev = [];
+
+    for (i = str.length; i >= 0; i--) {
+        rev += str[i]
+    }
+    return rev
+}
+
+console.log(revers("hello"));
+
+
