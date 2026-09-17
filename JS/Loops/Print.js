0e602ea9d55d36dbbc725b342f09b093f2cf55fa
Ayan_0011 <ayanansari9235@gmail.com>
2026-09-17 10:13:09 +0530

    ps in js

---
+++ JS/Loops/Print.js
@@ -0,0 +1,8 @@
+function PrintNo(n) {
+
+    for (let i = 1; i <= n; i++) {
+        console.log(i)
+    }
+}
+
+PrintNo(10)
---
+++ JS/Loops/PrintEvenNo.js
@@ -0,0 +1,9 @@
+function PrintEven(n) {
+    for (i = 0; i <= n; i++) {
+        if (i % 2 === 0) {
+            console.log(n)
+        }   
+    }
+}
+
+PrintEven(10);
