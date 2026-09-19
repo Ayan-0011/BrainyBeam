8919ad02d553edc51b3f283d9f2f7ba4eebd7e74
Ayan_0011 <ayanansari9235@gmail.com>
2026-09-19 16:16:03 +0530

    ps in js

---
+++ JS/Patterns/Pyramid.js
@@ -0,0 +1,15 @@
+let n = 5;
+
+for (let i = 1; i <= n; i++) {
+
+    //space 
+    for (let j = 1; j <= n - i; j++) {
+        process.stdout.write(" ")
+    }
+    //star
+    for (let k = 1; k <= 2 * i - 1; k++) {
+        process.stdout.write("*")
+    }
+    console.log();
+
+}
