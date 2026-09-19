2869bcebcbea76ea49c888c19db66ff7e829c29c
Ayan_0011 <ayanansari9235@gmail.com>
2026-09-19 15:05:12 +0530

    ps in js

--- JS/Patterns/p1.js
+++ JS/Patterns/p1.js
@@ -1,11 +1,29 @@
+
+//method 1 
 let n = 5;
 
 for (let i = 1; i <= n; i++) {
-    let row = "";
-
-    for (let j = 1; j <= n-1; j++) {
-        row += "*";
+    for (let j = 1; j <=i; j++) {
+        process.stdout.write(j.toString())
     }
 
-    console.log(row);
+    console.log();
 }
+
+//method 2
+// let n = 5;
+
+// for (let i = 1; i <= n; i++) {
+//     let row = "";
+//     for (let j = 1; j <= i; j++) {
+//         row += "*";
+//     }
+//     console.log(row);
+// }
+
+//output
+// 1
+// 12
+// 123
+// 1234
+// 12345
