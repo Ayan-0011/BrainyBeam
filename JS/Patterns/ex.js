b1c060c9247d895f07c306e5dd4aff490aa875cb
Ayan_0011 <ayanansari9235@gmail.com>
2026-09-21 09:31:36 +0530

    ps in js

--- JS/Patterns/ex.js
+++ JS/Patterns/ex.js
@@ -1,9 +1,13 @@
-let n = 10;
+let n = 5;
 
-for(let i=1; i<=n; i++){
-    for(let j=1; j<=i; j++){
-        process.stdout.write(j + "");
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i - 1; j++) {
+        process.stdout.write(" ");
+    }
+    for (let k = 1; k <= 2 * n - 2 * i + 1; k++) {
+        process.stdout.write("*");
+
     }
     console.log();
-    
+
 }
